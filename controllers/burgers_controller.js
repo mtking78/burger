27ca4d806db3b5db26bbf284ed633de20c1c6643
mtking78 <mtking78@gmail.com>
2018-04-23 23:29:09 -0700

    Initial files created.

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,4 @@
+// Import the following:
+    // Express
+    // burger.js
+// Create the router for the app, and export the router at the end of your file.
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+-- Select the burgers_db
+USE burgers_db;
+-- Insert new rows of data.
+INSERT INTO burgers (burger_name)
+VALUES ("hamburger"), ("cheeseburger"), ("tofu burger");
