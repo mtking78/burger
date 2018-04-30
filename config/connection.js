f0607aa7a834e3f04dff8c8ab5202bf4ad334f47
mtking78 <mtking78@gmail.com>
2018-04-29 20:31:17 -0700

    heroku deployment and jawsdb initialing

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 // Connect Node to MySQL.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "2631mountain",
-    database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "2631mountain",
+        database: "burgers_db"
+    });
+};
 
 connection.connect(function(err) {
     if (err) {
